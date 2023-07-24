bd777543b3d8a50e5d8e742708a5d479f2cae70f
umbertofaenza <umberto.faenza@gmail.com>
2023-07-24 16:17:28 +0200

    feat_js: initialize emails array

---
+++ mail project/js/main.js
@@ -0,0 +1,12 @@
+const emails = [
+  "example1@mail.com",
+  "example2@mail.com",
+  "example3@mail.com",
+  "example4@mail.com",
+  "example5@mail.com",
+  "example6@mail.com",
+  "example7@mail.com",
+  "example8@mail.com",
+  "example9@mail.com",
+  "example10@mail.com",
+];
