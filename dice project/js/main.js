58ba7244a8a0b3e389492ceb5932e12edcc5ea8e
umbertofaenza <umberto.faenza@gmail.com>
2023-07-24 17:17:04 +0200

    feat: game visible on dom

--- dice project/js/main.js
+++ dice project/js/main.js
@@ -1,15 +1,25 @@
 const numbers = [1, 2, 3, 4, 5, 6];
+const playButton = document.getElementById("play-btn");
+const userContainer = document.getElementById("user-num");
+const cpuContainer = document.getElementById("cpu-num");
+const resultContainer = document.getElementById("result-text");
 
-const userNumber = numbers[Math.floor(Math.random() * numbers.length)];
-console.log(userNumber);
+playButton.addEventListener("click", function () {
+  const userNumber = numbers[Math.floor(Math.random() * numbers.length)];
+  userContainer.innerText = userNumber;
 
-const cpuNumber = numbers[Math.floor(Math.random() * numbers.length)];
-console.log(cpuNumber);
+  const cpuNumber = numbers[Math.floor(Math.random() * numbers.length)];
+  cpuContainer.innerText = cpuNumber;
 
-if (userNumber > cpuNumber) {
-  console.log("User wins!");
-} else if (cpuNumber > userNumber) {
-  console.log("CPU wins!");
-} else {
-  console.log("It's a draw!");
-}
+  let resultMessage;
+
+  if (userNumber > cpuNumber) {
+    resultMessage = "User wins!";
+  } else if (cpuNumber > userNumber) {
+    resultMessage = "CPU wins!";
+  } else {
+    resultMessage = "It's a draw!";
+  }
+
+  resultContainer.innerText = resultMessage;
+});
