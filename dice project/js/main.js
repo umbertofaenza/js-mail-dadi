8d6577cc4c6f106736fc8a8f1d95ef326cf8fa6e
umbertofaenza <umberto.faenza@gmail.com>
2023-07-24 16:49:39 +0200

    feat_js: pick a number for pc and user

--- dice project/js/main.js
+++ dice project/js/main.js
@@ -1,1 +1,7 @@
 const numbers = [1, 2, 3, 4, 5, 6];
+
+const userNumber = numbers[Math.floor(Math.random() * numbers.length)];
+console.log(userNumber);
+
+const cpuNumber = numbers[Math.floor(Math.random() * numbers.length)];
+console.log(cpuNumber);
